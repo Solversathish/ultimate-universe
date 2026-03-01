c4a03680ce161b4dd32803b687489086d272fe75
sathish <sm7582537@gmail.com>
2026-03-01 18:06:24 +0530

    dbj

--- assets/js/world.js
+++ assets/js/world.js
@@ -51,7 +51,7 @@ function render(data) {
 
     card.innerHTML = `
       <div class="image-wrapper">
-        <img src="assets/images/${entity.image}">
+        <img src="assets/images/entities/${entity.image}">
       </div>
       <div class="card-title">${entity.name}</div>
     `;
