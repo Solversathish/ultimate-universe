8878ce01271c27d957f0647caf77258de3517889
sathish <sm7582537@gmail.com>
2026-03-27 11:56:19 +0530

    dn

--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -52,8 +52,8 @@ card.className="card";
 card.innerHTML=`
 <div class="image-wrapper">
 <img src="${getCDNImage(item.id,"thumb")}" loading="lazy">
-</div>
 <div class="card-title">${item.name}</div>
+</div>
 `;
 
 card.addEventListener("click",()=>{
