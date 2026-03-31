9132f5117561c238e43e5dbd3aacbae1d59b957e
sathish <sm7582537@gmail.com>
2026-03-31 10:53:37 +0530

    dn

--- assets/js/pages/category.js
+++ assets/js/pages/category.js
@@ -326,3 +326,22 @@ return str
 .replace(/\b\w/g,c=>c.toUpperCase())
 .trim();
 }
+
+/* ================= SCROLL TO TOP ================= */
+
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+
+scrollBtn?.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
--- assets/js/pages/home.js
+++ assets/js/pages/home.js
@@ -195,3 +195,22 @@ countElement.textContent =
 `${universes.length} items`;
 
 });
+
+/* ================= SCROLL TO TOP ================= */
+
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+});
+
+scrollBtn?.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
